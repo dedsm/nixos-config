854a8909bf8b04a24f96d2cc93bae1e5e4c55c73
David De Sousa <davidesousa@gmail.com>
2024-02-25 20:19:56 +0100

    Using nix-direnv

--- modules/home-manager/users/common/wayland/common/default.nix
+++ modules/home-manager/users/common/wayland/common/default.nix
@@ -13,6 +13,7 @@ attrs@{ lib, homeManagerConfig, unfreePkgs, pkgs, ... }: {
     direnv = {
       enable = true;
       enableZshIntegration = true;
+      nix-direnv.enable = true;
       stdlib = builtins.readFile ./direnvrc;
       config = { global = { warn_timeout = "3000h"; }; };
     };
