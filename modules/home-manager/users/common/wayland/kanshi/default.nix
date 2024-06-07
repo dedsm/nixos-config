f6ad3e0eb05764eca669d352fb21d75702227916
David De Sousa <davidesousa@gmail.com>
2024-06-07 10:28:40 +0200

    Update kanshi configuration

--- modules/home-manager/users/common/wayland/kanshi/default.nix
+++ modules/home-manager/users/common/wayland/kanshi/default.nix
@@ -2,77 +2,89 @@ attrs@{ lib, homeManagerConfig, unstablePkgs, pkgs, ... }: {
   services.kanshi = {
     enable = true;
     systemdTarget = "graphical-session.target";
-    profiles = {
-      default = {
-        outputs = [{
-          criteria = "eDP-1";
-          status = "enable";
-          scale = 1.566667;
-          position = "0,0";
-          mode = "2256x1504@59.999Hz";
-        }];
-      };
-      docked = {
-        outputs = [
-          {
+    settings = [
+      {
+        profile = {
+          name = "default";
+          outputs = [{
             criteria = "eDP-1";
             status = "enable";
             scale = 1.566667;
-            position = "960,1080";
-            mode = "2256x1504@59.999Hz";
-          }
-          {
-            criteria = "LG Electronics LG ULTRAFINE 110NTZN4L965";
-            status = "enable";
-            scale = 2.0;
             position = "0,0";
-            mode = "3840x2160@59.996Hz";
-          }
-          {
-            criteria = "LG Electronics LG ULTRAFINE 110NTMX4M035";
-            status = "enable";
-            scale = 2.0;
-            position = "1920,0";
-            mode = "3840x2160@59.996Hz";
-          }
-        ];
-      };
-      docked-downstairs = {
-        outputs = [
-          {
-            criteria = "eDP-1";
-            status = "enable";
-            scale = 1.566667;
-            position = "1920,0";
             mode = "2256x1504@59.999Hz";
-          }
-          {
-            criteria = "LG Electronics LG Ultra HD 0x0000E0D7";
-            status = "enable";
-            scale = 2.0;
-            position = "0,0";
-            mode = "3840x2160@60Hz";
-          }
-        ];
-      };
-      avr = {
-        outputs = [
-          {
-            criteria = "eDP-1";
-            status = "enable";
-            scale = 1.5;
-            position = "1920,0";
-            mode = "2256x1504@59.999Hz";
-          }
-          {
-            criteria = "Sony LG TV SSCR2 0x00000101";
-            status = "enable";
-            scale = 1.0;
-            position = "0,0";
-            mode = "1920x1080@60Hz";
-          }
-        ];
-      };
-    };
+          }];
+        };
+      }
+      {
+        profile = {
+          name = "docked";
+          outputs = [
+            {
+              criteria = "eDP-1";
+              status = "enable";
+              scale = 1.566667;
+              position = "960,1080";
+              mode = "2256x1504@59.999Hz";
+            }
+            {
+              criteria = "LG Electronics LG ULTRAFINE 110NTZN4L965";
+              status = "enable";
+              scale = 2.0;
+              position = "0,0";
+              mode = "3840x2160@59.996Hz";
+            }
+            {
+              criteria = "LG Electronics LG ULTRAFINE 110NTMX4M035";
+              status = "enable";
+              scale = 2.0;
+              position = "1920,0";
+              mode = "3840x2160@59.996Hz";
+            }
+          ];
+        };
+      }
+      {
+        profile = {
+          name = "docked-downstairs";
+          outputs = [
+            {
+              criteria = "eDP-1";
+              status = "enable";
+              scale = 1.566667;
+              position = "1920,0";
+              mode = "2256x1504@59.999Hz";
+            }
+            {
+              criteria = "LG Electronics LG Ultra HD 0x0000E0D7";
+              status = "enable";
+              scale = 2.0;
+              position = "0,0";
+              mode = "3840x2160@60Hz";
+            }
+          ];
+        };
+      }
+      {
+        profile = {
+          name = "avr";
+          outputs = [
+            {
+              criteria = "eDP-1";
+              status = "enable";
+              scale = 1.5;
+              position = "1920,0";
+              mode = "2256x1504@59.999Hz";
+            }
+            {
+              criteria = "Sony LG TV SSCR2 0x00000101";
+              status = "enable";
+              scale = 1.0;
+              position = "0,0";
+              mode = "1920x1080@60Hz";
+            }
+          ];
+        };
+      }
+    ];
   };
 }
