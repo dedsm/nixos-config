61f3340c954d710be3b4fe829a3913c4e2b01977
David De Sousa <davidesousa@gmail.com>
2025-04-17 11:44:49 +0200

    Moving to ruby lsp for nvim

--- modules/home-manager/users/common/nvim/config/lua/config/lsp/servers/ruby.lua
+++ modules/home-manager/users/common/nvim/config/lua/config/lsp/servers/ruby.lua
@@ -1,6 +1,5 @@
 local lspconfig = require('lspconfig')
 
-lspconfig.solargraph.setup {
-  on_attach = function(_, _) print('hello solargraph') end,
-  cmd = { "bundle", "exec", "solargraph", "stdio" }
+lspconfig.ruby_lsp.setup {
+  on_attach = function(_, _) print('hello lsp') end,
 }
