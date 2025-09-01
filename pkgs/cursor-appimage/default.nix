fbf54aa86be8d3e28c38115f51139d856a4602f5
David De Sousa <davidesousa@gmail.com>
2025-09-01 09:58:29 +0200

    Update cursor and flakes

--- pkgs/cursor-appimage/default.nix
+++ pkgs/cursor-appimage/default.nix
@@ -6,12 +6,12 @@
 
 let
   pname = "cursor";
-  version = "1.4.5";
+  version = "1.5.5";
 
   src = fetchurl {
-    url = "https://downloads.cursor.com/production/af58d92614edb1f72bdd756615d131bf8dfa5299/linux/x64/Cursor-1.4.5-x86_64.AppImage";
+    url = "https://downloads.cursor.com/production/823f58d4f60b795a6aefb9955933f3a2f0331d7b/linux/x64/Cursor-1.5.5-x86_64.AppImage";
     name = "cursor-${version}.AppImage";
-    sha256 = "sha256-2Hz1tXC+YkIIHWG1nO3/84oygH+wvaUtTXqvv19ZAz4=";
+    sha256 = "sha256-eq/AQcAONs6HBpRdx2yf4MFTQ1cm/rOeaZZTq8YUF8s=";
   };
 in
 appimageTools.wrapType2 {
