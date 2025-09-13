e415c7c59b63a8f54e0833b22453ac1c03fb492c
David De Sousa <davidesousa@gmail.com>
2025-09-13 15:32:44 +0200

    Showing dotfiles on vim

--- modules/home-manager/users/common/nvim/config/lua/config/neo_tree.lua
+++ modules/home-manager/users/common/nvim/config/lua/config/neo_tree.lua
@@ -6,6 +6,7 @@ require('neo-tree').setup({
     git_status_async = false,
     filesystem = {
         filtered_items = {
+            hide_dotfiles = false,
             hide_git_ignored = false,
         },
         use_libuv_file_watcher = true
