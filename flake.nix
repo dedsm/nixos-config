6bfa41ea87480fc20f7c28cf43c461fe1d7b0a51
David De Sousa <davidesousa@gmail.com>
2025-12-05 10:38:44 +0100

    chore: Update nix-darwin flake input to 25.05 branch.

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
     nixos-hardware = {url = "github:NixOS/nixos-hardware";};
     
     darwin = {
-      url = "github:lnl7/nix-darwin";
+      url = "github:lnl7/nix-darwin/nix-darwin-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
