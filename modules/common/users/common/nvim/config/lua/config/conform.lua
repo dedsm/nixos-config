2df02b26175644f91540ab8886a0da65e1074450
David De Sousa <davidesousa@gmail.com>
2026-03-07 18:10:38 +0100

    chore(nvim): switch markdown formatter from pandoc to mdformat

--- modules/common/users/common/nvim/config/lua/config/conform.lua
+++ modules/common/users/common/nvim/config/lua/config/conform.lua
@@ -4,17 +4,10 @@ conform.setup({
   formatters_by_ft = {
     python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
     ruby = { "rubocop" },
-    markdown = { "pandoc_markdown" },
+    markdown = { "mdformat" },
     nix = { "alejandra" },
   },
   formatters = {
-    pandoc_markdown = {
-      command = "pandoc",
-      -- A list of strings, or a function that returns a list of strings
-      -- Return a single string instead of a list to run the command in a shell
-      args = { "-f", "gfm", "-t", "gfm", "-s", "$FILENAME" },
-      stdin = true
-    },
     rubocop = {
       args = { "-a", "-f", "quiet", "--stderr", "--stdin", "$FILENAME" },
       cwd = function(ctx)
