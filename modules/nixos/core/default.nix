d012cac111b9747400e9dc82e6356aaaa2c102a6
David De Sousa <davidesousa@gmail.com>
2026-03-08 19:04:38 +0100

    feat(core): add Aegis Local CA certificate to system trust store

--- modules/nixos/core/default.nix
+++ modules/nixos/core/default.nix
@@ -48,5 +48,30 @@ in {
 
     security.polkit.enable = true;
     security.rtkit.enable = true;
+
+    # Aegis Local CA - private key has been deleted, safe to trust
+    security.pki.certificates = [
+      ''
+        -----BEGIN CERTIFICATE-----
+        MIIDEzCCAfugAwIBAgIUHaErDs5VmF4NS+nJXWJxfHT3mC8wDQYJKoZIhvcNAQEL
+        BQAwGTEXMBUGA1UEAwwOQWVnaXMgTG9jYWwgQ0EwHhcNMjYwMzA4MTYyNTU3WhcN
+        MjcwMzA4MTYyNTU3WjAZMRcwFQYDVQQDDA5BZWdpcyBMb2NhbCBDQTCCASIwDQYJ
+        KoZIhvcNAQEBBQADggEPADCCAQoCggEBAKiMHA0dc5owm/Ia6zJ8SJckdJlexOB1
+        +rmjkm4jZFp5BLH4P56zCy9uj/VTMPT78G8TY/7vY9tNc3xzkt6yDmW4z0IbAicy
+        AzDX28zGcID2bhQeyN1Jy/jYAMEjz5hMUgunlbas3+K99rNU2KGk0KgK5L84Ub4+
+        tOKmSkmKOw97LAP8PhOdRLmXHtbJMX8yNjU7w9dj0CHEDZdt4uJW7wThW4PRF8f8
+        JxcnIIsmqFCxy81NIyAG6zGz+xDOgIZ3q17Em2tBQFbPE3IoYC6fq7HzsJLrtaVi
+        NtN28xVQ7lqiibVcPie/TXU/D2BUHT8adMoVERAv5g652xuxlFfGiTECAwEAAaNT
+        MFEwHQYDVR0OBBYEFJbd1Ka7yZfkV5BeNunzi/zpNW5jMB8GA1UdIwQYMBaAFJbd
+        1Ka7yZfkV5BeNunzi/zpNW5jMA8GA1UdEwEB/wQFMAMBAf8wDQYJKoZIhvcNAQEL
+        BQADggEBABLL6JYzi5WW77jPazzMWLqyuTSfgkwzaK56bB0t2AE1ASRiE7kQ235g
+        jcJMfqBfDfv7BPb3AIgYmqTkPkmL+7Sg6NZsoYyEpUY0EZIA+PoiUxu7JMIVEwWw
+        iZYz5axg+ZHGDVrW+HZQ4RW9Nz93WY690DgxPGYNbx8crGE+pbB7QOFAL0HG0e9k
+        zswGnXsk1SOTNkhU1QxxvL/Hr0IHj5FAG2KmXT+Xq0Rnc+TnbuzYCGkeqkVjglKE
+        I4IeFxj5sowxitd7thb611Yii42t5TmQD/iuYQofqzvtySgni/GMp/dFZ00Ci7bR
+        Y7lfqYCiQmGXURI0qAd8hkjaE9iumQk=
+        -----END CERTIFICATE-----
+      ''
+    ];
   };
 }
