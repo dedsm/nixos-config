d29abed45eb6ba7a0c42363b000d9eb6f45efcd3
David De Sousa <davidesousa@gmail.com>
2026-05-19 19:40:11 +0200

    Update scaling on manwe dock setups and claude code and general flakes

--- modules/common/users/common/wayland/hyprdynamicmonitors/default.nix
+++ modules/common/users/common/wayland/hyprdynamicmonitors/default.nix
@@ -88,22 +88,22 @@ attrs@{ lib, homeManagerConfig, pkgs, hyprdynamicmonitors, ... }: {
         monitor=eDP-1, 2256x1504@59.999, 0x0, 1.333333
       '';
       "hyprdynamicmonitors/hyprconfigs/docked_lid_open.conf" = pkgs.writeText "docked_lid_open.conf" ''
-        monitor=desc:LG Electronics LG ULTRAFINE 110NTZN4L965, 3840x2160@59.996, 0x0, 2
-        monitor=desc:LG Electronics LG ULTRAFINE 110NTMX4M035, 3840x2160@59.996, 1920x0, 2
-        monitor=eDP-1, 2256x1504@59.999, 3840x0, 1.333333
+        monitor=desc:LG Electronics LG ULTRAFINE 110NTZN4L965, 3840x2160@59.996, 0x0, 1.5
+        monitor=desc:LG Electronics LG ULTRAFINE 110NTMX4M035, 3840x2160@59.996, 2560x0, 1.5
+        monitor=eDP-1, 2256x1504@59.999, 5120x0, 1.333333
       '';
       "hyprdynamicmonitors/hyprconfigs/docked_lid_closed.conf" = pkgs.writeText "docked_lid_closed.conf" ''
         monitor=eDP-1, disable
-        monitor=desc:LG Electronics LG ULTRAFINE 110NTZN4L965, 3840x2160@59.996, 0x0, 2
-        monitor=desc:LG Electronics LG ULTRAFINE 110NTMX4M035, 3840x2160@59.996, 1920x0, 2
+        monitor=desc:LG Electronics LG ULTRAFINE 110NTZN4L965, 3840x2160@59.996, 0x0, 1.5
+        monitor=desc:LG Electronics LG ULTRAFINE 110NTMX4M035, 3840x2160@59.996, 2560x0, 1.5
       '';
       "hyprdynamicmonitors/hyprconfigs/docked_downstairs_lid_open.conf" = pkgs.writeText "docked_downstairs_lid_open.conf" ''
-        monitor=desc:LG Electronics LG Ultra HD 0x0000E0D7, 3840x2160@60, 0x0, 2
-        monitor=eDP-1, 2256x1504@59.999, 1920x0, 1.333333
+        monitor=desc:LG Electronics LG Ultra HD 0x0000E0D7, 3840x2160@60, 0x0, 1.5
+        monitor=eDP-1, 2256x1504@59.999, 2560x0, 1.333333
       '';
       "hyprdynamicmonitors/hyprconfigs/docked_downstairs_lid_closed.conf" = pkgs.writeText "docked_downstairs_lid_closed.conf" ''
         monitor=eDP-1, disable
-        monitor=desc:LG Electronics LG Ultra HD 0x0000E0D7, 3840x2160@60, 0x0, 2
+        monitor=desc:LG Electronics LG Ultra HD 0x0000E0D7, 3840x2160@60, 0x0, 1.5
       '';
       "hyprdynamicmonitors/hyprconfigs/avr.conf" = pkgs.writeText "avr.conf" ''
         monitor=desc:Sony LG TV SSCR2 0x00000101, 1920x1080@60, 0x0, 1
