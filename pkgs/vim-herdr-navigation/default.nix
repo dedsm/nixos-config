62723f1fc2e587c000c14c3c0701d8e764f55b81
David De Sousa <davidesousa@gmail.com>
2026-08-23 19:51:29 +0200

    chore(vim-herdr-navigation): update pinned source to 2026-08-23

--- pkgs/vim-herdr-navigation/default.nix
+++ pkgs/vim-herdr-navigation/default.nix
@@ -18,13 +18,13 @@
 # module only has to register it. See docs/herdr.md.
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "vim-herdr-navigation";
-  version = "0.1.0-unstable-2026-08-02";
+  version = "0.1.0-unstable-2026-08-23";
 
   src = fetchFromGitHub {
     owner = "paulbkim-dev";
     repo = "vim-herdr-navigation";
-    rev = "820d48f5d9c9a7dece6a4bebfa3982ec30bbfbb7";
-    hash = "sha256-qn69GDH3kCSYm9x/it3EyJqZiwQoK3pnwdfATeSwJ38=";
+    rev = "79679dacc791f70fc34de8b29a3cf9706c0f5b2f";
+    hash = "sha256-iF0DLRn56eLGqY2iKTb3lX5iyVgl9CtSX5O2E5/pHjM=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
